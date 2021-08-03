0db80b7c219df870c58c252b5e61f2f51ccb7551
Mahbub Bello <mahbubbello@gmail.com>
2021-08-03 12:44:36 +0100

    added bootstrap and imported it into app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const morgan = require('morgan')
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 const app = express()
 
@@ -15,23 +16,50 @@ mongoose.connect('mongodb://localhost:27017/bloggr',
 //view engine
 app.set('view engine', 'ejs')
 
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: "Sunk cost fallacy",
+        snippet: "Lorem ipsum dolor sit amet",
+        body: "consectetur adipiscing elit. Quisque sapien velit, aliquet eget commodo nec, auctor a sapien. Nam eu neque vulputate diam rhoncus faucibus. Curabitur quis varius libero. Lorem"
+    })
+    blog.save()
+        .then((result)=> {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
 // middleware and static files
 app.use(express.static('public'))
 app.use(morgan('dev'))
 //send home page
 app.get('/', (req, res)=>{
-    const blogs = [
-        // {title: "Sunk cost fallacy", snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien velit, aliquet eget commodo nec, auctor a sapien. Nam eu neque vulputate diam rhoncus faucibus. Curabitur quis varius libero. Lorem.'},
-        // {title: "Confirmation bias", snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien velit, aliquet eget commodo nec, auctor a sapien. Nam eu neque vulputate diam rhoncus faucibus. Curabitur quis varius libero. Lorem.'},
-        // {title: "How to build web apps with node and express", snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien velit, aliquet eget commodo nec, auctor a sapien. Nam eu neque vulputate diam rhoncus faucibus. Curabitur quis varius libero. Lorem.'},
-
-    ]
+    res.redirect('/blogs')
     res.render('index', {title: 'Home', blogs })
 })
 
 //send about page
 app.get('/about', (req, res)=>{
     res.render('about', {title: 'About'})
+})
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 })
 app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Create new Blog'})
@@ -40,6 +68,3 @@ app.get('/blogs/create', (req, res) => {
 app.use((req, res) => {
     res.status(404).render('404', {title: 'Error: 404'})
 })
-//app listen
-
-
