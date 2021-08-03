9c174ad5500e1b0a1b8980f23707076543e292f5
Mahbub Bello <mahbubbello@gmail.com>
2021-08-03 07:36:50 +0100

    added statically generated content and rendered it on server side

--- app.js
+++ app.js
@@ -7,18 +7,24 @@ app.set('view engine', 'ejs')
 
 //send home page
 app.get('/', (req, res)=>{
-    res.render('index')
+    const blogs = [
+        {title: "Sunk cost fallacy", snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien velit, aliquet eget commodo nec, auctor a sapien. Nam eu neque vulputate diam rhoncus faucibus. Curabitur quis varius libero. Lorem.'},
+        {title: "Confirmation bias", snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien velit, aliquet eget commodo nec, auctor a sapien. Nam eu neque vulputate diam rhoncus faucibus. Curabitur quis varius libero. Lorem.'},
+        {title: "How to build web apps with node and express", snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sapien velit, aliquet eget commodo nec, auctor a sapien. Nam eu neque vulputate diam rhoncus faucibus. Curabitur quis varius libero. Lorem.'},
+
+    ]
+    res.render('index', {title: 'Home', blogs })
 })
 //send about page
 app.get('/about', (req, res)=>{
-    res.render('about')
+    res.render('about', {title: 'About'})
 })
 app.get('/blogs/create', (req, res) => {
-    res.render('create')
+    res.render('create', {title: 'Create new Blog'})
 })
 //404 page
 app.use((req, res) => {
-    res.status(404).render('404')
+    res.status(404).render('404', {title: 'Error: 404'})
 })
 //app listen
 app.listen(process.env.PORT || 3000, () => {
