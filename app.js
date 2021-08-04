b867d8f3f0607df8b10cd9946c6aaabb489118fa
Mahbub Bello <mahbubbello@gmail.com>
2021-08-04 07:30:50 +0100

    added route handlers to routes file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogRoutes = require('./routes/blogRoutes')
 
 const app = express()
 
@@ -15,33 +15,14 @@ mongoose.connect('mongodb://localhost:27017/bloggr',
 
 //view engine
 app.set('view engine', 'ejs')
-
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: "Sunk cost fallacy",
-        snippet: "Lorem ipsum dolor sit amet",
-        body: "consectetur adipiscing elit. Quisque sapien velit, aliquet eget commodo nec, auctor a sapien. Nam eu neque vulputate diam rhoncus faucibus. Curabitur quis varius libero. Lorem"
-    })
-    blog.save()
-        .then((result)=> {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
 // middleware and static files
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
+
+//blog routes
+app.use('/blogs', blogRoutes)
+
 //send home page
 app.get('/', (req, res)=>{
     res.redirect('/blogs')
@@ -52,18 +33,7 @@ app.get('/', (req, res)=>{
 app.get('/about', (req, res)=>{
     res.render('about', {title: 'About'})
 })
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result})
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create new Blog'})
-})
+
 //404 page
 app.use((req, res) => {
     res.status(404).render('404', {title: 'Error: 404'})
