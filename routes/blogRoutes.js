5760559b2b4d74280833f47532b341082f501d86
Mahbub Bello <mahbubbello@gmail.com>
2021-08-04 09:36:50 +0100

    completed the MVC implementation by adding a controllers.js to handle all routes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,53 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const Blog = require('../models/blog')
+const blogController = require('../controllers/blogController')
 
 
-router.post('/', (req, res) => {
-    // console.log(req.body);
-    const blog = new Blog(req.body);
 
-    blog.save()
-        .then(result => {
-            res.redirect('/blogs');
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-router.get('/create', (req, res) => {
-    res.render('create', {title: 'Create new Blog'})
-})
+router.get('/', blogController.blog_index)
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { blog: result, title: 'Blog details' })
-        })
-        .catch(err => {
-            console.log(err)
-        })
-})
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
+router.post('/', blogController.blog_create_post)
+    
+router.get('/create', blogController.blog_create_get)
 
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/blogs' });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-router.get('/', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result})
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
+router.get('/:id', blogController.blog_details)
+
+router.delete('/:id', blogController.blog_delete);
+
 
 module.exports = router
